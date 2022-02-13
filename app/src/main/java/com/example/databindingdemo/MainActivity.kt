67a5a0f60b89bb3e2db1a765fddebc94375467a9
YapHongKiat <yaphk-wm19@student.tarc.edu.my>
2022-02-13 18:04:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/databindingdemo/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.databindingdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databindingdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val s:Student = Student("W123456", "Ali")
+        binding.student = s
+
+        binding.btnUpdate.setOnClickListener(){
+            s.name = "Alex"
+
+            binding.apply{
+                invalidateAll()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/databindingdemo/Student.kt
@@ -0,0 +1,5 @@
+package com.example.databindingdemo
+
+data class Student(var id:String = "",
+                   var name:String = ""
+)
